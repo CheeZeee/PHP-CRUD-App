d346f0e082efa4431503fcb457f63963f72d4f22
Chika Chikwendu <Chika Chikwendu>
2017-01-21 02:10:24 +0100

    Alter sql file

--- dev/products.sql
+++ dev/products.sql
@@ -21,3 +21,4 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `created`, `modifi
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `created`, `modified`) VALUES (5, 'Pillow', 'Use this to spice up your nightsleep', 11.50, '2016-12-21 17:04:03', '2016-12-27 12:05:03');
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `created`, `modified`) VALUES (6, 'Earphones', 'This is very important to people who love music', 25.50, '2016-12-21 11:04:03', '2016-12-27 12:05:03');
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `created`, `modified`) VALUES (7, 'PC', 'Used for development, work and fun stuffs', 100.25, '2017-01-21', '2017-01-22 00:43:00');
